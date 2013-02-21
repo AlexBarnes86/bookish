4669907615453c47bc38215b5193b83b9dd88595
Alex Barnes <AlexBarnes86@gmail.com>
2013-02-20 23:19:37 -0600

    Added neo4j server

--- src/main/java/com/toastedbits/bookish/repositories/CategoryRepository.java
+++ src/main/java/com/toastedbits/bookish/repositories/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.toastedbits.bookish.repositories;
 
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.stereotype.Repository;
 
 import com.toastedbits.bookish.domain.Category;
 
 @Repository
-public interface CategoryRepository extends GraphRepository<Category> {}
+public interface CategoryRepository extends GraphRepository<Category> {
+	@Query("start cat=node:Category match (Category)<-(Category)")
+	public Iterable<Category> getTree(Category root);
+}
